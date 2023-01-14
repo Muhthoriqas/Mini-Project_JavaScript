cc3d7f47a572073d112ccba2b4034faf0488a16e
Thoriq As <72277295+Muhthoriqas@users.noreply.github.com>
2023-01-15 07:00:22 +0800

    Add Quote

--- Quote/quote.js
+++ Quote/quote.js
@@ -28,7 +28,6 @@ async function updateQuote() {
   // Fetch a random quote from the Quotable API
   const response = await fetch("https://api.quotable.io/random");
   const data = await response.json();
-  console.log(data);
 
   if (response.ok) {
     // Update DOM elements
