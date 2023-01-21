e35741833095344cf6c9f5d1bd0cbfb85635232e
Thoriq As <72277295+Muhthoriqas@users.noreply.github.com>
2023-01-22 01:07:26 +0800

    Update Logic

--- Country/country.js
+++ Country/country.js
@@ -1,29 +1,61 @@
-async function getCountryName(code) {
+var inputUser = document.getElementById("county-input");
+var resultDisplay = document.getElementById("result");
+const btn = document.getElementById("btn");
+const body = document.querySelector("#body");
+const loader = document.querySelector("#loading");
+
+const displayLoading = () => {
+  body.style.opacity = 0.5;
+  loader.classList.add("display");
+  // to stop loading after 5 second
+  setTimeout(() => {
+    loader.classList.remove("display");
+  }, 5000);
+};
+
+const hideLoading = () => {
+  body.style.opacity = 1;
+  loader.classList.remove("display");
+};
+
+async function getCountryName() {
+  let code = inputUser.value;
+
   async function getData(pageNumber) {
+    displayLoading();
     const url =
       "https://jsonmock.hackerrank.com/api/countries?page=" + pageNumber;
 
     const response = await axios.get(url);
-    const data = response.data;
+    const allData = response.data;
 
-    const currentPage = data.page;
-    const totalPages = data.total_pages;
+    const currentPage = allData.page;
+    const totalPages = allData.total_pages;
 
     var resultCountry = null;
 
-    data.data.forEach((country) => {
-      if (country.alpha2Code === code) {
-        resultCountry = country.name;
+    allData.data.forEach((country) => {
+      if (country.alpha2Code.toLowerCase() === code.toLowerCase()) {
+        resultCountry = "Country Name: <br /> " + country.name;
+      } else if (country.name.toLowerCase() === code.toLowerCase()) {
+        resultCountry = "Country Code: <br /> " + country.alpha2Code;
       }
     });
 
-    if (!resultCountry && currentPage < totalPages) {
-      return await getData(currentPage + 1);
+    if (!resultCountry && currentPage <= totalPages) {
+      if (currentPage === totalPages && resultCountry === null) {
+        resultDisplay.innerHTML = "Can't Find Country";
+        hideLoading();
+      } else {
+        return await getData(currentPage + 1);
+      }
     } else {
-      console.log(resultCountry);
-      return resultCountry;
+      hideLoading();
+      resultDisplay.innerHTML = resultCountry;
     }
   }
 
   return await getData(0);
 }
+
+btn.addEventListener("click", getCountryName);
