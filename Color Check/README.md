bcd8102712c20b0ab7a59ce4ec8569e0e09dc4c7
Thoriq As <72277295+Muhthoriqas@users.noreply.github.com>
2023-01-30 01:41:20 +0800

    add color check

---
+++ Color Check/README.md
@@ -0,0 +1,3 @@
+# This is my answer from gogoblogan assignment
+
+# gogoblesan-eps-1 url youtube: https://youtu.be/YDcmSjxYyP0
---
+++ Color Check/main.js
@@ -0,0 +1,20 @@
+const targetObject = document.getElementById("targetObject");
+const inputHex = document.getElementById("inputHexa");
+
+const ubahLatar = (inputColor) => {
+  inputColor = inputHex.value;
+
+  // Cek CSS Color like red, green, blue , etc
+  var s = new Option().style;
+  s.color = inputColor;
+  var cssColor = s.color == inputColor;
+
+  // Cek Hex Color
+  var colorRegex =
+    /^(#[0-9A-Fa-f]{3}|#[0-9A-Fa-f]{6}|rgb\(\d{1,3},\s\d{1,3},\s\d{1,3}\)|hsl\(\d{1,3},\s\d{1,3}%,\s\d{1,3}%\))$/;
+  var hexColor = colorRegex.test(inputColor);
+
+  cssColor == true || hexColor == true
+    ? (targetObject.style.backgroundColor = inputColor)
+    : alert("Input is not a Color");
+};
